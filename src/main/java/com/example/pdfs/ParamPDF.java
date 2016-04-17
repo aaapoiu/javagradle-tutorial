a7d0e9680fba47fd37157940d38e705ea8dea8fd
hyo <leehj08@coupang.com>
2016-04-18 00:15:58 +0900

    ./gradlew run -Pname=value

---
+++ src/main/java/com/example/pdfs/ParamPDF.java
@@ -0,0 +1,30 @@
+package com.example.pdfs;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+public class ParamPDF {
+    public static final String RESULT = "my.pdf";
+    public static final String message = "hello world!";
+
+    public static void main(String[] args) throws DocumentException, IOException {
+	if (args.length == 0) {
+	    new ParamPDF().createPdf(RESULT, message);
+	} else {
+	    new ParamPDF().createPdf(RESULT, args[0]);
+	}
+    }
+
+    public void createPdf(String filename, String word) throws DocumentException, IOException {
+	Document document = new Document();
+	PdfWriter.getInstance(document, new FileOutputStream(filename));
+	document.open();
+	document.add(new Paragraph(word));
+	document.close();
+    }
+}
